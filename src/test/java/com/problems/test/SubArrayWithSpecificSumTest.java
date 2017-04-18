fed774ea1711c326f1e15b361084f7fdf1e2aaf5
Max <max.iakovliev@gmail.com>
2017-04-18 22:23:41 +0800

    solving problems... added one more test

--- src/test/java/com/problems/test/SubArrayWithSpecificSumTest.java
+++ src/test/java/com/problems/test/SubArrayWithSpecificSumTest.java
@@ -16,6 +16,13 @@ public class SubArrayWithSpecificSumTest {
         Integer[] input={ 4, 2, -3, -1, 0, 4 };
         List<List<Integer>> result= s.Solution1(input, 0);
         Assert.assertEquals(2,result.size());
+    }
 
+    @Test
+    public void TestSolution1Case2(){
+        SubArrayWithSpecificSum s=new SubArrayWithSpecificSum();
+        Integer[] input={ 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 };
+        List<List<Integer>> result= s.Solution1(input, 0);
+        Assert.assertEquals(6,result.size());
     }
 }
