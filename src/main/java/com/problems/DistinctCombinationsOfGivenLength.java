2a4f918f89828281e6797bf7354dfdb065e347d3
Max <max.iakovliev@gmail.com>
2017-04-29 09:52:41 +0800

    solving problems... added one more test

--- src/main/java/com/problems/DistinctCombinationsOfGivenLength.java
+++ src/main/java/com/problems/DistinctCombinationsOfGivenLength.java
@@ -24,6 +24,4 @@ public class DistinctCombinationsOfGivenLength {
         return  result;
     }
 
-
-
 }
--- src/test/java/com/problems/test/DistinctCombinationsOfGivenLengthTest.java
+++ src/test/java/com/problems/test/DistinctCombinationsOfGivenLengthTest.java
@@ -1,14 +1,32 @@
 package com.problems.test;
 
 import com.problems.DistinctCombinationsOfGivenLength;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by Maks on 4/25/2017.
  */
 public class DistinctCombinationsOfGivenLengthTest {
 
+    @Test
     public  void solution1TestCase1(){
         DistinctCombinationsOfGivenLength s=new DistinctCombinationsOfGivenLength();
         Integer[] input=new Integer[]{1, 2, 3};
+        List<List<Integer>> res= s.solutionForLen2(input);
+        List<Integer[]> expected =new ArrayList<Integer[]>();
+        expected.add(new Integer[]{1, 2});
+        expected.add(new Integer[]{1, 3});
+        expected.add(new Integer[]{2, 3});
+
+        for(int i=0;i< expected.size(); i++){
+            for(int j=0; j<expected.get(i).length; j++){
+                Assert.assertEquals(expected.get(i)[j], res.get(i).get(j));
+            }
+        }
     }
 }
