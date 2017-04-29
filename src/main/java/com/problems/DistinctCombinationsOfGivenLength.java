f67a93fa605dd53cb5b2723f0bbd87a64be3bec6
Max <max.iakovliev@gmail.com>
2017-04-29 09:40:11 +0800

    DistinctCombinationsOfGivenLength refined

--- src/main/java/com/problems/DistinctCombinationsOfGivenLength.java
+++ src/main/java/com/problems/DistinctCombinationsOfGivenLength.java
@@ -10,26 +10,20 @@ import java.util.List;
  */
 public class DistinctCombinationsOfGivenLength {
 
-    public List<List<Integer>> solution1(Integer[] arr, int len) {
+    public List<List<Integer>> solutionForLen2(Integer[] arr) {
+
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        int counter=0;
-        //отсортировать
-        while(counter<arr.length-len){//пока самый первый элемент не достиг конца массива со сдвигом на длину
-            for(int i=len-1; i>=0; i++){
-                List<List<Integer>> cur=collect(arr, i, len, counter);
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length; j++){
+                List<Integer> cur=new ArrayList<Integer>();
+                cur.add(arr[i]);
+                cur.add(arr[j]);
+                result.add(cur);
             }
         }
-
         return  result;
     }
 
-    private List<List<Integer>> collect(Integer[] arr, int pos, int len, int shift) {
-        List<List<Integer>> result=new ArrayList<List<Integer>>();
-        for(int i=0; i<len-1; i++){
 
-        }
-
-        return  result;
-    }
 
 }
