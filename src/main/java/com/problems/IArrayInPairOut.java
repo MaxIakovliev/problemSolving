06eae4b5f013906680e330480771ead41b0bb880
Max <max.iakovliev@gmail.com>
2017-05-02 21:50:46 +0800

    refactoring, Initial implementation of maximum produc subarray

---
+++ src/main/java/com/problems/IArrayInPairOut.java
@@ -0,0 +1,11 @@
+package com.problems;
+
+import javafx.util.Pair;
+
+/**
+ * Created by Maks on 5/2/2017.
+ * refactoring, just for fun
+ */
+public interface IArrayInPairOut {
+    Pair<Integer,Integer> solution1(Integer[] arr);
+}
--- src/main/java/com/problems/MaximumProductSubarray.java
+++ src/main/java/com/problems/MaximumProductSubarray.java
@@ -1,14 +1,35 @@
 package com.problems;
 
-import java.util.List;
+import javafx.util.Pair;
 
 /**
  * Created by Maks on 5/2/2017.
+ * http://www.techiedelight.com/find-maximum-product-subarray-given-array/
  */
-public class MaximumProductSubarray implements IArrayInputSolution {
+public class MaximumProductSubarray implements IArrayInPairOut {
 
 
-    public List<Integer> solution1(Integer[] arr) {
-        return null;
+    public Pair<Integer,Integer> solution1(Integer[] arr) {
+        int curMax=0;
+        int curMin=0;
+        int maxResult=0;
+        int left=0;
+        int right=0;
+
+
+        for(int i=0; i<arr.length; i++){
+            int temp=curMax;
+            curMax=Math.max(arr[i],Math.max(arr[i]*curMax, curMin));
+            curMin=Math.min(arr[i]*temp, arr[i]*curMin);
+            if(curMax>maxResult){
+                left=i;
+                right=i;
+                maxResult=curMax;
+            }
+            else {
+                right = i;
+            }
+        }
+        return new Pair<Integer, Integer>(left, right);
     }
 }
