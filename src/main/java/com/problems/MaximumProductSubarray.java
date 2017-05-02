090c7bcaa167d558287533b8e178fd60276795e6
Max <max.iakovliev@gmail.com>
2017-05-02 21:26:27 +0800

    small refactoring ;)

---
+++ src/main/java/com/problems/MaximumProductSubarray.java
@@ -0,0 +1,14 @@
+package com.problems;
+
+import java.util.List;
+
+/**
+ * Created by Maks on 5/2/2017.
+ */
+public class MaximumProductSubarray implements IArrayInputSolution {
+
+
+    public List<Integer> solution1(Integer[] arr) {
+        return null;
+    }
+}
