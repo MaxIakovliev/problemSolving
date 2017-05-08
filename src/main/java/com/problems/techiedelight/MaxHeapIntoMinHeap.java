92a07a334a2e7473a2c1e622905f341068f0c67c
Max <max.iakovliev@gmail.com>
2017-05-08 21:59:26 +0800

    solving problems... added one more test

---
+++ src/main/java/com/problems/techiedelight/MaxHeapIntoMinHeap.java
@@ -0,0 +1,44 @@
+package com.problems.techiedelight;
+
+/**
+ * Created by Maks on 5/8/2017.
+ * http://www.techiedelight.com/convert-max-heap-min-heap-linear-time/
+ */
+public class MaxHeapIntoMinHeap {
+    public  void solution1(Integer[] arr){
+
+    }
+
+    private void heapify(Integer[] arr, int i, int size){
+        int l=left(i);
+        int r=right(i);
+        int min=arr[i];
+        if(l<size &&arr[l].compareTo(arr[i])<0 ){
+            min=l;
+        }
+
+        if(r<size && arr[r].compareTo(arr[l])<0){
+            min=r;
+        }
+        if(min!=i){
+            swap(arr,i,min);
+            heapify(arr,min,size);
+        }
+
+    }
+
+    private void swap(Integer[] arr, int i, int min) {
+        int tmp=arr[i];
+        arr[i]=arr[min];
+        arr[min]=tmp;
+    }
+
+
+    private int left(int x){
+        return  2*x;
+    }
+
+    private int right(int x){
+        return  2*x+1;
+    }
+}
