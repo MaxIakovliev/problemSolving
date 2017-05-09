a7dda19866de4f15a954e1d83c84e8ff514e7459
Max <max.iakovliev@gmail.com>
2017-05-09 21:03:39 +0800

    uMerge M sorted lists using minheap

--- src/main/java/com/problems/techiedelight/MergeMSortedLists.java
+++ src/main/java/com/problems/techiedelight/MergeMSortedLists.java
@@ -1,6 +1,8 @@
 package com.problems.techiedelight;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Created by Maks on 5/9/2017.
@@ -11,7 +13,7 @@ public class MergeMSortedLists {
     /*
     1. create new array  the size of summary of size of all lists
     2. copy all the elements into new array
-    3. apply efficient sort  O(Nlogn)
+    3. apply efficient sort  O(Nlog(n))
      */
     public  Integer[] simpleSolution(Integer[][] arr){
         int size=0;
@@ -28,5 +30,58 @@ public class MergeMSortedLists {
         }
         Arrays.sort(result);
         return  result;
+    }
+
+    /*
+    construct a min-heap of size M and insert first element of each list into it.
+    Then we pop root element (minimum element) from the heap and insert next element
+    from “same” list as popped element. We repeat this process till the heap is exhausted.
+     Complexity O(Nlog(M))
+     */
+    public Integer[] minHeapSolution(Integer[][] arr){
+        int size=0;
+        for(int i=0; i<arr.length; i++){
+            size+=arr[i].length;
+        }
+        Integer[] result=new Integer[size];
+        IntComparator comparator=new IntComparator();
+        PriorityQueue<Integer> pq=new PriorityQueue<Integer>(size, comparator);
+        int counter=0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j< arr[i].length; j++){
+                result[counter+j]=arr[i][j];
+            }
+            counter+=arr[i].length;
+        }
+        counter=0;
+        int j=0;
+        while(j<size) {
+            for (int i = 0; i < arr.length; i++) {
+                if(counter<arr[i].length){
+                    pq.add(arr[i][counter]);
+                    j++;
+                }
+            }
+            counter++;
+        }
+
+        counter=0;
+        while(!pq.isEmpty()){
+            result[counter]=pq.poll();
+            counter++;
+        }
+        return  result;
+
+    }
+
+    public class IntComparator implements Comparator<Integer>{
+
+        public int compare(Integer o1, Integer o2) {
+            if(o1<o2)
+                return  -1;
+            if(o1>o2)
+                return  1;
+            else return  0;
+        }
     }
 }
