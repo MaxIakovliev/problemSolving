a46dc83cba23afb9c69007335f4d43ea29be1c8f
Max <max.iakovliev@gmail.com>
2017-05-10 22:13:47 +0800

    one more unit test

--- src/test/java/com/problems/techiedelight/test/SmallestRangeInAllListsTest.java
+++ src/test/java/com/problems/techiedelight/test/SmallestRangeInAllListsTest.java
@@ -25,4 +25,20 @@ public class SmallestRangeInAllListsTest  implements  com.problems.test.ISingleT
         Assert.assertEquals(exp.getValue(),res.getValue());
 
     }
+
+    @Test
+    public void solution1TestCase2() {
+        SmallestRangeInAllLists sr=new SmallestRangeInAllLists();
+        Integer[][] input=new Integer[4][];
+        input[0]=new Integer[]{2, 3, 4, 8, 10, 15};
+        input[1]=new Integer[]{1, 5, 12 };
+        input[2]=new Integer[]{7, 8, 15, 16};
+        input[3]=new Integer[]{3, 6  };
+
+        Pair<Integer, Integer> res= sr.solution1(input);
+        Pair<Integer, Integer> exp=new Pair<Integer, Integer>(4,7);
+        Assert.assertEquals(exp.getKey(),res.getKey());
+        Assert.assertEquals(exp.getValue(),res.getValue());
+
+    }
 }
