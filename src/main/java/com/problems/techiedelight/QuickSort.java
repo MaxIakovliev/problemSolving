85cde835e56a4f2e8070d1f53f93bd570014e746
Max <max.iakovliev@gmail.com>
2017-05-13 13:50:06 +0800

    quickSort initial implementation

---
+++ src/main/java/com/problems/techiedelight/QuickSort.java
@@ -0,0 +1,36 @@
+package com.problems.techiedelight;
+
+/**
+ * Created by Maks on 5/13/2017.
+ */
+public class QuickSort {
+    private int partition(Integer[] arr, int lo, int hi){
+
+        int pivot=arr[hi];
+        int i=lo-1;
+        for(int j=lo; j<hi-1; j++){
+            if(arr[j]<=pivot){
+                i++;
+                swap(arr, i,j);
+            }
+        }
+
+        swap(arr,i+1, hi);
+
+        return  i+1;
+    }
+
+    private void swap(Integer[] arr, int i, int j) {
+        int tmp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=tmp;
+    }
+
+    public void sort(Integer[] arr, int lo, int hi){
+        if(lo<hi) {
+            int pivotal = partition(arr, lo, hi);
+            sort(arr,lo, pivotal-1);
+            sort(arr,pivotal+1, hi);
+        }
+    }
+}
