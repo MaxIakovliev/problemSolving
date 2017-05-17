864f6c4dd12427aba4a01bbd03d6163e33b2a488
Max <max.iakovliev@gmail.com>
2017-05-17 21:48:09 +0800

    solving problems... added one more test

---
+++ src/test/java/com/problems/techiedelight/test/FindNumberOfRotationsTest.java
@@ -0,0 +1,20 @@
+package com.problems.techiedelight.test;
+
+import com.problems.techiedelight.FindNumberOfRotations;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Maks on 5/17/2017.
+ */
+public class FindNumberOfRotationsTest {
+
+    @Test
+    public void linearSolutionTestCase1(){
+        FindNumberOfRotations fr=new FindNumberOfRotations();
+        Integer[] input={8, 9, 10, 2, 5, 6};
+        int res= fr.linearSolution(input);
+        int exp=3;
+        Assert.assertEquals(exp,res);
+    }
+}
