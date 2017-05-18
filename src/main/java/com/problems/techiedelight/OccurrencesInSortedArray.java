9c764e968a4b49738890ac17ce0e00c3e4cb8f10
Max <max.iakovliev@gmail.com>
2017-05-18 22:07:57 +0800

    Count occurrences of a number in a sorted array with duplicates

---
+++ src/main/java/com/problems/techiedelight/OccurrencesInSortedArray.java
@@ -0,0 +1,47 @@
+package com.problems.techiedelight;
+
+/**
+ * Created by Maks on 5/18/2017.
+ * http://www.techiedelight.com/count-occurrences-number-sorted-array-duplicates/
+ */
+public class OccurrencesInSortedArray {
+
+    public int count(Integer[] arr, int x){
+        int lo=0;
+        int hi=arr.length-1;
+        int res=0;
+        int idx=-1;
+        boolean isLeft=true;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(arr[mid]==x && idx==-1){
+                idx=mid;
+                res++;
+                hi=mid-1;
+            }
+            else if(arr[mid]==x && isLeft && idx!=-1){
+                res++;
+                hi=mid-1;
+            }
+            else  if(arr[mid]==x && !isLeft && idx !=-1){
+                res++;
+                lo=mid+1;
+
+            }
+            else if(x>arr[mid] && idx!=-1){
+                isLeft=false;
+                lo=idx+1;
+            }
+            else if(x<arr[mid] && idx!=-1){
+                return  res;
+            }
+            else if(x<arr[mid]&& idx==-1){
+                hi=mid-1;
+            }
+            else if(x>arr[mid] && idx==-1){
+                lo=mid+1;
+            }
+        }
+        return  res;
+    }
+}
