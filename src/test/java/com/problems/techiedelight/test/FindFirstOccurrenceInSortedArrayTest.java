9b9cdb8cab24efa17d58988f59f7cc3d164f1c5d
Max <max.iakovliev@gmail.com>
2017-05-18 21:46:11 +0800

    added unit test

---
+++ src/test/java/com/problems/techiedelight/test/FindFirstOccurrenceInSortedArrayTest.java
@@ -0,0 +1,21 @@
+package com.problems.techiedelight.test;
+
+import com.problems.techiedelight.FindFirstOccurrenceInSortedArray;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Maks on 5/18/2017.
+ */
+public class FindFirstOccurrenceInSortedArrayTest {
+
+    @Test
+    public void solution1TestCase1(){
+        FindFirstOccurrenceInSortedArray s=new FindFirstOccurrenceInSortedArray();
+        Integer[] input={2, 5, 5, 5, 6, 6, 8, 9, 9, 9};
+        int exp=1;
+        int res=s.findFirstOccurrence(input, 5);
+        Assert.assertEquals(exp, res);
+
+    }
+}
