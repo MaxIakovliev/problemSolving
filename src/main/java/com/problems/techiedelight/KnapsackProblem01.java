5877c77d01d71c3dc2fcf88d47b8131c79624328
Max <max.iakovliev@gmail.com>
2017-05-22 23:07:21 +0800

    0-1 Knapsack Problem - recursive solution

---
+++ src/main/java/com/problems/techiedelight/KnapsackProblem01.java
@@ -0,0 +1,22 @@
+package com.problems.techiedelight;
+
+/**
+ * Created by Maks on 5/22/2017.
+ * http://www.techiedelight.com/0-1-knapsack-problem/
+ */
+public class KnapsackProblem01 {
+    public int recursiveSolution(Integer[] weights, Integer[] vals,int size,  int limit){
+        if(size<0 || limit==0) {
+            return 0;
+        }
+
+        if(limit<0){
+            return Integer.MIN_VALUE;
+        }
+
+        int include=vals[size]+recursiveSolution(weights,vals,size-1,limit-vals[size]);
+        int exclude=recursiveSolution(weights,vals,size-1, limit);
+
+        return Math.max(include,exclude);
+    }
+}
