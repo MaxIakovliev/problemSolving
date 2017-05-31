9398d2016a06913804e70e9bea7b54cadb93b09f
Max <max.iakovliev@gmail.com>
2017-05-31 21:01:59 +0800

    LongestCommonSubsequence

---
+++ src/main/java/com/problems/geeksforgeeks/LongestCommonSubsequence.java
@@ -0,0 +1,28 @@
+package com.problems.geeksforgeeks;
+
+/**
+ * Created by Maks on 5/31/2017.
+ * http://www.geeksforgeeks.org/dynamic-programming-set-4-longest-common-subsequence/
+ */
+public class LongestCommonSubsequence {
+
+    /*
+    find length of longest common subsequence in recursive way
+     */
+    public  int recursiveCount(char[] x, char[] y, int xs, int ys){
+
+        if(xs==0 || ys==0){
+            return  0;
+        }
+        if(x[xs]==y[ys]){
+            return 1+recursiveCount(x,y,xs-1, ys-1);
+        }
+        else {
+            return Math.max(
+                    recursiveCount(x,y,xs-1, ys),
+                    recursiveCount(x,y,xs,ys-1)
+            );
+        }
+
+    }
+}
