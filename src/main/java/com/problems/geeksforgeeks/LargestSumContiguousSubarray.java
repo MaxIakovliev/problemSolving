60445139477e45516ed3c3ec88c3230afc08c78a
Max <max.iakovliev@gmail.com>
2017-06-04 20:04:36 +0800

    kadanesAlgorithm extended - return; 1 max sum start pos end pos

--- src/main/java/com/problems/geeksforgeeks/LargestSumContiguousSubarray.java
+++ src/main/java/com/problems/geeksforgeeks/LargestSumContiguousSubarray.java
@@ -21,4 +21,39 @@ public class LargestSumContiguousSubarray {
         }
         return  max;
     }
+
+    public ResultTuple kadanesAlgorithmExt(int[]arr){
+        ResultTuple res=new ResultTuple();
+        int max=Integer.MIN_VALUE;
+        int maxEndHere=0;
+        int startPos=0;
+        int endPos=0;
+        int tmpStartPos=0;
+
+        for(int i=0; i<arr.length; i++){
+            maxEndHere+=arr[i];
+            if(max<maxEndHere){
+                max=maxEndHere;
+                endPos=i;
+                if(tmpStartPos>0){
+                    startPos=tmpStartPos;
+                }
+            }
+
+            if(maxEndHere<0){
+                maxEndHere=0;
+                tmpStartPos=i;
+            }
+        }
+        res.sum=max;
+        res.startPos=startPos;
+        res.endPos=endPos;
+        return  res;
+    }
+
+    private class ResultTuple{
+        public int sum;
+        public int startPos;
+        public int endPos;
+    }
 }
