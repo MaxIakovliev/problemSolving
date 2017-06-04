d417aaed7eda5d50783f11eb4c4f7c29019820ad
Max <max.iakovliev@gmail.com>
2017-06-04 23:41:44 +0800

    min cost path + unit test

---
+++ src/test/java/com/problems/geeksforgeeks/test/MinCostPathTest.java
@@ -0,0 +1,22 @@
+package com.problems.geeksforgeeks.test;
+
+import com.problems.geeksforgeeks.MinCostPath;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Maks on 6/4/2017.
+ */
+public class MinCostPathTest {
+
+    @Test
+    public  void naiveRecursiveSolutionTestCase1(){
+        MinCostPath mcp=new MinCostPath();
+        int[][]arr={{1, 2, 3},
+                    {4, 8, 2},
+                    {1, 5, 3} };
+        int res = mcp.naiveRecursiveSolution(arr,2,2);
+        int exp=8;
+        Assert.assertEquals(exp,res);
+    }
+}
