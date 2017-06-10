a56f393de0410c78ff012e20e4d9d98cb3515cd1
Max <max.iakovliev@gmail.com>
2017-06-10 17:32:55 +0800

    Minimum  number of jump to the end - initial implementation

---
+++ src/main/java/com/problems/geeksforgeeks/MinimumJumpToTheEnd.java
@@ -0,0 +1,25 @@
+package com.problems.geeksforgeeks;
+
+/**
+ * Created by Maks on 6/10/2017.
+ * http://www.geeksforgeeks.org/minimum-number-of-jumps-to-reach-end-of-a-given-array/
+ */
+public class MinimumJumpToTheEnd {
+
+    public  int recursiveSolution(int[] arr, int s, int e){
+        if (s==e){
+            return 0;
+        }
+        if(arr[s]==0){
+            return  Integer.MAX_VALUE-1;
+        }
+
+        int min=Integer.MAX_VALUE;
+        for(int i=s+1; i<=e && i<=s+arr[s]; i++){
+            int jump=recursiveSolution(arr, s,e);
+            min=Math.min(min, jump);
+
+        }
+        return  min;
+    }
+}
