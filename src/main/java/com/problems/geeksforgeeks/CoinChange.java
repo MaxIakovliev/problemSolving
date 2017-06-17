49b6fa96cdb80f5804bfb68e170d41e56f6d1d5a
Max <max.iakovliev@gmail.com>
2017-06-17 21:27:15 +0800

    update

--- src/main/java/com/problems/geeksforgeeks/CoinChange.java
+++ src/main/java/com/problems/geeksforgeeks/CoinChange.java
@@ -9,32 +9,25 @@ import java.util.Arrays;
 public class CoinChange {
 
     public int recursiveSolution(int[] coins, int size, int total){
-        if(total==0){
-            return 1;
-        }
+       if(total==0)
+           return 1;
+       if(total<0|| size <=0)
+           return 0;
 
-        if(total<0){
-            return  0;
-        }
-
-        if(size<=0 && total>=1){
-            return  0;
-        }
-
-        return  recursiveSolution(coins, size-1, total)+recursiveSolution(coins, size, total-coins[size-1]);
+       return recursiveSolution(coins, size-1, total)+ recursiveSolution(coins, size, total-coins[size-1]);
     }
 
     public  int iterativeSolution(int[] coins, int size, int total){
-        int[] table=new int[total+1];
-        Arrays.fill(table,0);
-        table[0]=1;
-        //walk through all the coins
+        int[] tmp=new int[total+1];
+        Arrays.fill(tmp,0);
+        tmp[0]=1;
+
         for(int i=0; i<size; i++){
             for(int j=coins[i]; j<=total; j++){
-                table[j]+=table[j-coins[i]];
+                tmp[j]+=tmp[j-coins[i]];
             }
         }
-        return  table[total];
+        return tmp[total];
 
     }
 
--- src/main/java/com/problems/geeksforgeeks/EditDistance.java
+++ src/main/java/com/problems/geeksforgeeks/EditDistance.java
@@ -8,32 +8,21 @@ public class EditDistance {
 
     public int recursiveSolution(String  s1, String s2, int x,int y){
 
-        // If first string is empty, the only option is to
-        // insert all characters of second string into first
-        if(x==0){
+        //if s1 is empty- we have to use whole s2 string
+        if(x==0)
             return y;
-        }
+        //if s2 is empty we have to use whole s1 string
+        if(y==0)
+            return x;
 
-        // If second string is empty, the only option is to
-        // remove all characters of first string
-        if(y==0){
-            return  x;
-        }
-
-        // If last characters of two strings are same, nothing
-        // much to do. Ignore last characters and get count for
-        // remaining strings.
-        if(s1.charAt(x-1)==s2.charAt(y-1)){
-            return  recursiveSolution(s1,s2,x-1,y-1);
-        }
-
-        // If last characters are not same, consider all three
-        // operations on last character of first string, recursively
-        // compute minimum cost for all three operations and take
-        // minimum of three values.
-        return 1+ min(  recursiveSolution(s1,s2,x-1,y-1),//Replace
-                        recursiveSolution(s1,s2,x-1,y),//Remove
-                        recursiveSolution(s1,s2,x,y-1));//Insert
+        if(s1.charAt(x-1)==s2.charAt(y-1))
+            return recursiveSolution(s1,s2,x-1,y-1);
+        else
+            return 1+min(
+                    recursiveSolution(s1,s2, x-1, y-1),
+                    recursiveSolution(s1,s2, x-1, y),
+                    recursiveSolution(s1,s2,x,y-1)
+            );
 
 
     }
