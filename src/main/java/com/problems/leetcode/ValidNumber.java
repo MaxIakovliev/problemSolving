d6a51e2640f787f53268e5f8cc0b7726f74eeac5
Max <max.iakovliev@gmail.com>
2017-07-20 01:47:40 +0800

    valid number  - final solution + unit tests

--- src/main/java/com/problems/leetcode/ValidNumber.java
+++ src/main/java/com/problems/leetcode/ValidNumber.java
@@ -5,44 +5,117 @@ package com.problems.leetcode;
  */
 public class ValidNumber {
     public boolean isNumber(String s) {
+
         boolean preSpace=true;
         int numberStarted=0;
         boolean xStarted=false;
+        boolean xClosedByDigit=false;
         boolean binStarted=false;
+        boolean binClosedByDigit=false;
+        boolean greatherZero=false;
+        boolean signPassed=false;
+        boolean eSignPassed=false;
+        boolean dotPassed=false;
+        boolean dotClosedByDigit=false;
+        boolean ePassed=false;
+        boolean eClosedByDigit=false;
+        boolean ePlusPassed=false;
+        boolean firstZero=false;
+
         if(s.length()==0)
             return  false;
 
         for(int i=0; i<s.length(); i++){
             char c=s.charAt(i);
 
-            if(preSpace && numberStarted==0 && c==' ')
+            if(numberStarted==0 && Character.isDigit(c) && c=='0'){
+                firstZero=true;
+            }
+
+            if(!binStarted && Character.isDigit(c) && c!='0'){//  special helper for bin value
+                greatherZero=true;
+            }
+
+            if(preSpace && numberStarted==0 &&!dotPassed && c==' ') {
                 continue;
-            else if((preSpace && c=='-') || (preSpace && c=='+')
-                    ||(preSpace && Character.isDigit(c)) ||(preSpace && c=='.')){
+            }
+
+            else if((preSpace && !signPassed && !dotPassed && c=='-') || (preSpace &&!signPassed &&!dotPassed && c=='+')){
+                signPassed=true;
+                preSpace=false;
+            }
+            else if(ePassed && dotPassed && !eSignPassed && (c=='-'|| c=='+') && i!=s.length()-1){
+                eSignPassed=true;
+            }
+            else if(ePassed &&  !eSignPassed && (c=='-'|| c=='+') && i!=s.length()-1){
+                eSignPassed=true;
+            }
+            else if (!dotPassed &&numberStarted<=1 && !ePassed && !binStarted && !xStarted && c == '.') {
+                dotPassed=true;
+            }
+            else if(numberStarted==0 && Character.isDigit(c)){
                 preSpace=false;
                 numberStarted=1;
+                if(dotPassed){
+                    dotClosedByDigit=true;
+                }
             }
-            else if(!preSpace && numberStarted==1 &&Character.isDigit(c))
-                continue;
-            else if(!preSpace && numberStarted==1 && !xStarted  && !binStarted &&(c=='x' ||c=='X')){
+
+
+            else if(!preSpace && numberStarted==1 && !xStarted  && !binStarted  &&!ePassed && (c=='e'||c=='E')){
+                ePassed=true;
+            }
+            else if(!preSpace && numberStarted==1 && !xStarted  && !binStarted  &&ePassed  && !ePlusPassed && c=='+'){
+                ePlusPassed=true;
+                eClosedByDigit=false;
+            }
+            else if(!preSpace && numberStarted==1 && !xStarted  && !binStarted  &&dotPassed  && Character.isDigit(c)){
+                dotClosedByDigit=true;
+                if(ePassed)
+                    eClosedByDigit=true;
+            }
+            else if(!preSpace && numberStarted==1 && !xStarted  && !binStarted  &&ePassed  && Character.isDigit(c)){
+                eClosedByDigit=true;
+            }
+            else if(!preSpace && numberStarted==1 && xStarted  && !binStarted  &&!ePassed  && Character.isDigit(c)){
+                xClosedByDigit=true;
+            }
+
+            else if(!preSpace && numberStarted==1 && !xStarted  && !binStarted && !greatherZero &&(c=='x' ||c=='X')){
+
                 xStarted=true;
             }
-            else if(!preSpace && numberStarted==1 && xStarted && !binStarted &&
+            else if(!preSpace && numberStarted==1 && !binStarted &&Character.isDigit(c)) {
+                continue;
+            }
+            else if(!preSpace && numberStarted==1 && xStarted && !binStarted &&!dotPassed &&
                     (Character.isDigit(c) ||c=='a' || c=='A' || c=='b'|| c=='B'|| c=='c'|| c=='C'
                             || c=='d'|| c=='D'|| c=='e'|| c=='E'||c=='f'||c=='F')) {
-                continue;
+                xClosedByDigit=true;
             }
             else if(!preSpace && numberStarted==1 && !xStarted && !binStarted &&(c=='l' ||c=='L' || c=='f' ||c=='F')){
                 numberStarted=2;
+
+                if(firstZero && (c=='l' ||c=='L') )
+                    return  false; ///handling stupid 07l case of leetcode
+                if(dotPassed && (c=='l' ||c=='L'))
+                    return  false;
+                if((c=='f' ||c=='F'))
+                    return  false;
             }
             else if(!preSpace && numberStarted==1 && !xStarted && !binStarted && (c=='b' || c=='B')){
+
+                if(greatherZero){ //special check of history
+                    return false;
+                }
+
                 binStarted=true;
             }
             else if(!preSpace && numberStarted==1 && !xStarted && binStarted &&(c=='1'||c=='0')){
-                continue;
+                binClosedByDigit=true;
             }
             else if(!preSpace && numberStarted>0 && c==' '){
-                continue;
+                numberStarted=2;
             }
             else
                 return  false;
@@ -50,6 +123,18 @@ public class ValidNumber {
             float f1= .1f;
 
         }
+        if( binStarted && !binClosedByDigit)
+            return false;
+
+        if (xStarted && !xClosedByDigit)
+            return  false;
+
+        if (dotPassed && numberStarted==0 && !dotClosedByDigit)
+            return  false;
+
+        if(ePassed && !eClosedByDigit)
+            return  false;
+
         if(numberStarted>0)
             return  true;
         else
--- src/test/java/com/problems/leetcode/ValidNumberTest.java
+++ src/test/java/com/problems/leetcode/ValidNumberTest.java
@@ -11,31 +11,49 @@ public class ValidNumberTest {
     @Test
     public  void testValidIntNumbers(){
         ValidNumber vn=new ValidNumber();
-        Assert.assertEquals(true,vn.isNumber("1"));
-        Assert.assertEquals(true,vn.isNumber("-1"));
-        Assert.assertEquals(true,vn.isNumber("+1"));
-        Assert.assertEquals(true,vn.isNumber(" 1"));
-        Assert.assertEquals(true,vn.isNumber("1 "));
-        Assert.assertEquals(true,vn.isNumber(" 1 "));
-        Assert.assertEquals(true,vn.isNumber(" -1 "));
-        Assert.assertEquals(true,vn.isNumber(" +1 "));
-        Assert.assertEquals(true,vn.isNumber(" 11 "));
-        Assert.assertEquals(true,vn.isNumber(" 112 "));
-        Assert.assertEquals(true,vn.isNumber("   112 "));
-        Assert.assertEquals(true,vn.isNumber("   112   "));
-        Assert.assertEquals(true,vn.isNumber("9876543210"));
-        Assert.assertEquals(true,vn.isNumber("987654321023456789"));
-        Assert.assertEquals(true,vn.isNumber("987654321023456789  "));
+        Assert.assertEquals("1",true,vn.isNumber("1"));
+        Assert.assertEquals("2",true,vn.isNumber("-1"));
+        Assert.assertEquals("3",true,vn.isNumber("+1"));
+        Assert.assertEquals("4",true,vn.isNumber(" 1"));
+        Assert.assertEquals("5",true,vn.isNumber("1 "));
+        Assert.assertEquals("6",true,vn.isNumber(" 1 "));
+        Assert.assertEquals("7",true,vn.isNumber(" -1 "));
+        Assert.assertEquals("8",true,vn.isNumber(" +1 "));
+        Assert.assertEquals("9",true,vn.isNumber(" 11 "));
+        Assert.assertEquals("10",true,vn.isNumber(" 112 "));
+        Assert.assertEquals("11",true,vn.isNumber("   112 "));
+        Assert.assertEquals("12",true,vn.isNumber("   112   "));
+        Assert.assertEquals("13",true,vn.isNumber("9876543210"));
+        Assert.assertEquals("14",true,vn.isNumber("987654321023456789"));
+        Assert.assertEquals("15",true,vn.isNumber("987654321023456789  "));
+    }
+
+    @Test
+    public  void testValidHexNumbers(){
+        ValidNumber vn=new ValidNumber();
+        int x=0xaafffa;
+        Assert.assertEquals("1",true,vn.isNumber("0x0"));
+        Assert.assertEquals("2",true,vn.isNumber("0x1"));
+        Assert.assertEquals("3",true,vn.isNumber("0x1233455"));
+        Assert.assertEquals("4",true,vn.isNumber("0x9a"));
+        Assert.assertEquals("5",true,vn.isNumber("0xaa9a"));
+        Assert.assertEquals("6",true,vn.isNumber("0xaafffa"));
+        Assert.assertEquals("7",true,vn.isNumber("0xaaFFFa"));
+        Assert.assertEquals("8",true,vn.isNumber("-0xaaFFFa"));
+        Assert.assertEquals("9",true,vn.isNumber("+0xaaFFFa"));
     }
 
     @Test
     public  void testInvalidIntNumbers(){
         ValidNumber vn=new ValidNumber();
         Assert.assertEquals("1",false,vn.isNumber(""));
+        Assert.assertEquals("11",false,vn.isNumber("459277e38+"));
+        Assert.assertEquals("1",false,vn.isNumber("671.l"));
         Assert.assertEquals("1",false,vn.isNumber("1-"));
         Assert.assertEquals("1.1",false,vn.isNumber("1+"));
         Assert.assertEquals("1.2",false,vn.isNumber("1+"));
         Assert.assertEquals("1.2",false,vn.isNumber("1+1"));
+        Assert.assertEquals("1.22",false,vn.isNumber("1 1"));
         Assert.assertEquals("2",false,vn.isNumber(" "));
         Assert.assertEquals("3",false,vn.isNumber("! "));
         Assert.assertEquals("4",false,vn.isNumber(" q "));
@@ -69,8 +87,92 @@ public class ValidNumberTest {
 
     @Test
     public  void testInvalidBinNumbers(){
-        int b=0b0001;
+        int b=0b1;
         ValidNumber vn=new ValidNumber();
-        Assert.assertEquals(false,vn.isNumber("00b1"));
+        Assert.assertEquals("1",false,vn.isNumber("01b1"));
+        Assert.assertEquals("2",false,vn.isNumber("20b1"));
+        Assert.assertEquals("3",false,vn.isNumber("0b12"));
+        Assert.assertEquals("4",false,vn.isNumber("0b000002"));
+        Assert.assertEquals("5",false,vn.isNumber("0 0b01"));
+        Assert.assertEquals("5",false,vn.isNumber("0b0 1"));
+        Assert.assertEquals("5",false,vn.isNumber("0b0x1"));
+        Assert.assertEquals("5",false,vn.isNumber("0b0f1"));
     }
-}
+
+
+    @Test
+    public  void testValidFloatNumbers() {
+        double b = .0;
+        ValidNumber vn = new ValidNumber();
+        Assert.assertEquals("1",true, vn.isNumber(".0"));
+        /*Assert.assertEquals("1",true, vn.isNumber("3."));
+        Assert.assertEquals("1.1",true, vn.isNumber("1.0f"));
+        Assert.assertEquals("2",true, vn.isNumber("1.000f"));
+        Assert.assertEquals("3",true, vn.isNumber(".000f"));
+        Assert.assertEquals("4",true, vn.isNumber(".00033f"));
+        Assert.assertEquals("5",true, vn.isNumber(" .00033f"));
+        Assert.assertEquals("6", true, vn.isNumber(" .00033f "));
+        Assert.assertEquals("7",true, vn.isNumber(".00033f "));
+        Assert.assertEquals("8",true, vn.isNumber("-.00033f "));
+        Assert.assertEquals("9",true, vn.isNumber("+.00033f "));
+        Assert.assertEquals("10",true, vn.isNumber(" +.00033f "));
+        Assert.assertEquals("11",true, vn.isNumber(" -.00033f "));*/
+    }
+
+    @Test
+    public  void testValidScientificNumbers() {
+        Integer b = (int)1e0;
+
+        ValidNumber vn = new ValidNumber();
+        Assert.assertEquals("1", true, vn.isNumber("166e-02767"));
+        Assert.assertEquals("1", true, vn.isNumber("32.e-23"));
+        Assert.assertEquals("1", true, vn.isNumber("46.e3"));
+        Assert.assertEquals("1", true, vn.isNumber("1e0"));
+        Assert.assertEquals("1", true, vn.isNumber("1e+0"));
+        Assert.assertEquals("1", true, vn.isNumber("1e+000"));
+        Assert.assertEquals("1", true, vn.isNumber("10e+000"));
+        Assert.assertEquals("1", true, vn.isNumber("+10e+000"));
+        Assert.assertEquals("1", true, vn.isNumber("-10e+000"));
+        Assert.assertEquals("1", true, vn.isNumber(" -10e+000"));
+        Assert.assertEquals("1", true, vn.isNumber("  -10e+000"));
+        Assert.assertEquals("1", true, vn.isNumber(" -10e+000  "));
+    }
+
+    @Test
+    public  void testIncorrectNumbers() {
+        float b =959440.94f;
+
+        ValidNumber vn = new ValidNumber();
+        Assert.assertEquals("1", false, vn.isNumber("-788f"));
+        Assert.assertEquals("1", false, vn.isNumber("2e+60++604"));
+        Assert.assertEquals("1", false, vn.isNumber("459277e38+"));
+        Assert.assertEquals("1", false, vn.isNumber("6e6.5"));
+        Assert.assertEquals("1.11", false, vn.isNumber("0x."));
+        Assert.assertEquals("1.112", false, vn.isNumber("07L"));
+        Assert.assertEquals("1.1", false, vn.isNumber("0x"));
+        Assert.assertEquals("1.11", false, vn.isNumber("0x 0"));
+        Assert.assertEquals("1.2", false, vn.isNumber("0b"));
+        Assert.assertEquals("1", false, vn.isNumber(".-4"));
+        Assert.assertEquals("1", false, vn.isNumber("."));
+        Assert.assertEquals("1", false, vn.isNumber("1x11"));
+        Assert.assertEquals("1.1", false, vn.isNumber(". 0"));
+        Assert.assertEquals("1.2", false, vn.isNumber("- 0"));
+        Assert.assertEquals("1.3", false, vn.isNumber(" - 0"));
+        Assert.assertEquals("1.4", false, vn.isNumber(" .e"));
+        Assert.assertEquals("1.5", false, vn.isNumber("1 ."));
+        Assert.assertEquals("2", false, vn.isNumber(" ."));
+        Assert.assertEquals("3", false, vn.isNumber(" . "));
+        Assert.assertEquals("4", false, vn.isNumber(" + "));
+        Assert.assertEquals("5", false, vn.isNumber(" - "));
+        Assert.assertEquals("6", false, vn.isNumber("0.0."));
+        Assert.assertEquals("7", false, vn.isNumber("0.."));
+        Assert.assertEquals("8", false, vn.isNumber("..0"));
+        Assert.assertEquals("9", false, vn.isNumber("e"));
+        Assert.assertEquals("10", false, vn.isNumber("x"));
+        Assert.assertEquals("11", false, vn.isNumber("f"));
+        Assert.assertEquals("11.1", false, vn.isNumber("0e"));
+        Assert.assertEquals("11.2", false, vn.isNumber("0e+"));
+        Assert.assertEquals("11", false, vn.isNumber("10e+000-"));
+    }
+
+    }
